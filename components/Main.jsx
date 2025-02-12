1c07e19206653121f488b98990df896fa77e19e5
hrh4real <himanshuhada167@gmail.com>
2025-02-12 11:32:16 +0530

    Updated Main.jsx and index.css

--- components/Main.jsx
+++ components/Main.jsx
@@ -29,14 +29,37 @@ export default function AddIngredients() {
                     name="ingredient"
                 />
                 <button
-                    
+                    className="add-ingredient-button"
                 >
                     Add Ingredient
                 </button>
             </form>
-            <ul>
-                {ingredientsList}
-            </ul>
+            {ingredientsList.length ? (
+                <section className="get-recipe-section">
+                <h3>Ingredients on hand : </h3>
+                <ul className="ingredients-list" aria-live="polite">
+                    {ingredientsList}
+                </ul>
+                {
+                    ingredientsList.length > 3 ? (
+                        <div className="get-recipe-container">
+                            <div>
+                                <h4>Ready for a recipe</h4>
+                                <p>Generate a recipe from your list of ingredients.</p>
+                            </div>
+                            <div>
+                            <button
+                                className="get-recipe-button"
+                            >
+                                Get a recipe
+                            </button>
+                            </div>
+                        </div>
+                    ) : null
+                }
+            </section>
+            ) : null}
+
         </main>
     )
 }
